a81f9d7af065a0c7b33cdd015066403040ab2690
kotireddythamatam <48517249+kotireddythamatam@users.noreply.github.com>
2019-11-04 13:10:34 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,2 +1,3 @@
 this is first python file.
 python is a open source language and it is maintained by python org foundation that is non profitable organinzatin.
+this is  third line and it is used for merging purpose.
